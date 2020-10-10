5e5c4a0c71b19d65ac72658fec8f1b844d381821
Paul Schaaf <paulsimonschaaf@gmail.com>
2020-10-10 22:02:16 +0200

    added dead code and deps

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # solchess
 Chess game on Solana
 
-possible optimisations:
+possible optimisations/additions:
+    - add turn timers and game length
+    - play for crypto, winner gets input
     - improve legal_chess (see its readme)
     - game_arr could be stored in less bytes because you don't need an entire byte to save a piece
--- src/program-rust/src/lib.rs
+++ src/program-rust/src/lib.rs
@@ -62,42 +62,6 @@ fn process_instruction(
     Ok(())
 }
 
-// Sanity tests
-#[cfg(test)]
-mod test {
-    use super::*;
-    use solana_sdk::clock::Epoch;
-
-    #[test]
-    fn test_sanity() {
-        let program_id = Pubkey::default();
-        let key = Pubkey::default();
-        let mut lamports = 0;
-        let mut data = vec![0; mem::size_of::<u64>()];
-        LittleEndian::write_u64(&mut data, 0);
-        let owner = Pubkey::default();
-        let account = AccountInfo::new(
-            &key,
-            false,
-            true,
-            &mut lamports,
-            &mut data,
-            &owner,
-            false,
-            Epoch::default(),
-        );
-        let instruction_data: Vec<u8> = Vec::new();
-
-        let accounts = vec![account];
-
-        assert_eq!(LittleEndian::read_u64(&accounts[0].data.borrow()), 0);
-        process_instruction(&program_id, &accounts, &instruction_data).unwrap();
-        assert_eq!(LittleEndian::read_u64(&accounts[0].data.borrow()), 1);
-        process_instruction(&program_id, &accounts, &instruction_data).unwrap();
-        assert_eq!(LittleEndian::read_u64(&accounts[0].data.borrow()), 2);
-    }
-}
-
 // Required to support info! in tests
 #[cfg(not(target_arch = "bpf"))]
 solana_sdk::program_stubs!();
